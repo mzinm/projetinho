299e74a095ff97ff66b4db114de8cec11b2f9e77
mzinm <miguel.willians.silva1234@gmail.com>
2024-10-11 10:35:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criarCartao(
+    'programaçâo',
+    'o que é puthon',
+    'o pythin é uma linguagem de promação'
+)
+criarCartao(
+    'programaçâo',
+    'o que é puthon',
+    'o pythin é uma linguagem de promação'
+)
+criarCartao(
+    'programaçâo',
+    'o que é puthon',
+    'o pythin é uma linguagem de promação'
+)
