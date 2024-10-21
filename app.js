9fc15d38e9ef4ffb42df7f06772fd74da2376620
mzinm <miguel.willians.silva1234@gmail.com>
2024-10-21 16:38:52 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 function criaCartao(categoria, pergunta, resposta){
-console.log(categoria, pergunta, resposta)
+let container = document.getElementById ('container')
+let cartao = document.createElement ('article')
+cartao.className = 'cartao'
+cartao.innerHTML = `
+<div class="cartao__conteudo">
+<h3>Programação</h3>
+<div class="cartao__conteudo__pergunta">
+<p>o que é javascript?</p>
+</div>
+<div class="cartao__conteudo__resposta">
+<p>o javascript é uma linguagem de programação</p>
+</div>
+</div>
+`
+container.appendChild(cartao)
 }
